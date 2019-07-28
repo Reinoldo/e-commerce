a94bce0c0b855875abe8f908ad62fd4498d5345d
Reinoldo Sommer <reinoldo@outlook.com>
2019-07-28 20:19:15 +0100

    File add to simulate a back-end. Few changes to accept the data

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -1,9 +1,38 @@
 <template>
-  <div></div>
+  <section class="products-container">
+    <div v-for="product in products" :key="product.id">
+      <img v-if="product.photos" :src="product.photos[0].src" :alt="product.photos[0].titulo" />
+      <h2 class="titulo">{{product.name}}</h2>
+      <p class="preco">{{product.price}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import { api } from "@/services.js";
+export default {
+  data() {
+    return {
+      products: null
+    };
+  },
+  methods: {
+    getProducts() {
+      api
+        .get("/product")
+        .then(result => {
+          this.products = result.data;
+        })
+        .catch(err => {
+          //eslint-disable-next-line
+          console.log(err);
+        });
+    }
+  },
+  created() {
+    this.getProducts();
+  }
+};
 </script>
 
 <style>
